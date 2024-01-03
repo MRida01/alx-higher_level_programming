ed121c8539e6a113a91e0db521da050475865e6d
Mohamed Rida Oukili <lolridalol@gmail.com>
2024-01-02 18:14:51 -0800

    updates

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str.split(" ")[-8:-5], end=" ")
+print(str[72:121] + str[0:6])
+print(str)
