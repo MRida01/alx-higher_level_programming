7ea0621768445ba205448994a01d5695e5fc6a89
Mohamed Rida Oukili <lolridalol@gmail.com>
2024-01-03 17:43:22 -0800

    updates

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(s):
+    for char in s:
+        upper_char = chr(ord(char) - (ord('a') - ord('A'))) if 'a' <= char <= 'z' else char
+        print("{}".format(upper_char), end="")
+    print()
+
+if __name__ == "__main__":
+    uppercase("best")
+    uppercase("Best School 98 Battery street")
