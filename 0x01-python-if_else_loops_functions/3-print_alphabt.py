f892313b9de6bcb79a39f796db2ee89398f69547
Mohamed Rida Oukili <lolridalol@gmail.com>
2024-01-03 17:30:35 -0800

    updates

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 
-print("".join(chr(char) for char in range(ord('a'), ord('z') + 1) if chr(char) not in 'qe'), end="")
+print("".join([chr(char) for char in range(ord('a'), ord('z') + 1) if chr(char) not in 'qe']).format(), end="")
