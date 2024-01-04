b67fdde4765961f2d598246b13e485208a71c7db
Mohamed Rida Oukili <lolridalol@gmail.com>
2024-01-03 17:53:31 -0800

    updates

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def fizzbuzz():
-
-
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz", end=" ")
@@ -11,6 +9,7 @@ def fizzbuzz():
             print("Buzz", end=" ")
         else:
             print(i, end=" ")
+
 
 if __name__ == "__main__":
     fizzbuzz()
