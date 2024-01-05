94997e4178f8046f06d55d2495ebb362c130225c
Mohamed Rida Oukili <lolridalol@gmail.com>
2024-01-04 16:55:05 -0800

    updates

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 a = 1
 b = 2
-from add_0 import add
+add_0 = __import__('add_0').add
 
-print("{} + {} = {}".format(a, b, add(a, b)))
+print("{:d} + {:d} = {:d}".format(a, b, add_0(a, b)))
