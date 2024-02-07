5a0bebad0a3133d8320489f74642c66b3438e0cc
Mohamed Rida Oukili <lolridalol@gmail.com>
2024-02-07 15:10:55 -0800

    updates

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""Module to read a text file and print its contents to stdout."""
+
 def read_file(filename=""):
+    """Reads a text file and prints its contents to stdout.
+
+    Args:
+        filename (str): The path to the text file to be read. Defaults to "".
+
+    Returns:
+        None
+    """
     with open(filename, 'r', encoding='utf-8') as file:
         print(file.read(), end="")
