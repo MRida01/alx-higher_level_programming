f177da1b7831bee8f10fdb0fb73b91b69d3de533
Mohamed Rida Oukili <lolridalol@gmail.com>
2024-02-07 15:19:01 -0800

    updates

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module that returns an object represented by a JSON string."""
+
+import json
+
+def from_json_string(my_str):
+    """Returns an object represented by a JSON string.
+
+    Args:
+        my_str (str): The JSON string representing the object.
+
+    Returns:
+        object: The Python data structure represented by the JSON string.
+    """
+    return json.loads(my_str)
