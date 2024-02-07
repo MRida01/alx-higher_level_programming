a0e9e6b5bab3d5982c550771411283631da15fb7
Mohamed Rida Oukili <lolridalol@gmail.com>
2024-02-07 15:26:39 -0800

    updates

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Module that adds all arguments to a Python list, and then save them to a file."""
+
 import sys
 from 5-save_to_json_file import save_to_json_file
 from 6-load_from_json_file import load_from_json_file
