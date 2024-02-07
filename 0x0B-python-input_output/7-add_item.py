1e88e769d89363c672fb710b9448f88faebbfbe4
Mohamed Rida Oukili <lolridalol@gmail.com>
2024-02-07 15:32:44 -0800

    updates

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python3
-"""Module that adds all arguments to a Python list, and then save them to a file."""
+"""
+Module: 7-add_item
+
+This module provides a script to add all command-line arguments to a Python list
+and save them to a file in JSON format.
+
+Usage:
+    ./7-add_item.py [argument1] [argument2] ...
+
+Arguments:
+    - [argument1], [argument2], ... : Any number of arguments to be added to the list.
+
+The list is saved as a JSON representation in a file named add_item.json. If the file
+does not exist, it will be created.
+
+Functions:
+    No functions are directly exposed by this module.
+"""
 
 import sys
 from 5-save_to_json_file import save_to_json_file
