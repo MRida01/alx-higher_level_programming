e92ed90ff23e8307fe84dba05fd94136b16b86c8
Mohamed Rida Oukili <lolridalol@gmail.com>
2024-02-07 18:17:41 -0800

    updates

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module that generates a rectangle class."""
+
+class Rectangle(BaseGeometry):
+    """Rectangle class inheriting from BaseGeometry."""
+
+    def __init__(self, width, height):
+        """Initialize the Rectangle with width and height."""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        """Return a string representation of the Rectangle."""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def area(self):
+        """Calculate the area of the Rectangle."""
+        return self.__width * self.__height
