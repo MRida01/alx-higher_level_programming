e5647911a8c7610f1e1eb7a6bb00904287cac086
Mohamed Rida Oukili <lolridalol@gmail.com>
2024-05-21 18:36:03 -0700

    updates

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,18 +1,18 @@
 #!/usr/bin/node
 
-const fs = require('fs');
-// Import built-in Node.js 'fs' module.
+'use strict';
+
+const fs = require('fs'); // Import the built-in Node.js 'fs' module.
 
 fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  // Use fs.readFile() to read contents of the file specified as a command-line argument
-  // 'utf8' specifies encoding of the file being read
+  // Use fs.readFile() to read the contents of a file specified as a command-line argument.
+  // 'utf8' specifies the encoding of the file being read.
 
   if (error) {
-    // If an error occurs during the file read operation, 'error' parameter will contain an error object.
+    // If an error occurs during the file read operation, the 'error' parameter will contain an error object.
     console.error('Error reading the file:', error);
-
   } else {
-    // If file is read successfully, the 'content' parameter will contain contents of the file as a string.
+    // If the file is read successfully, the 'content' parameter will contain the contents of the file as a string.
     console.log(content);
   }
 });
