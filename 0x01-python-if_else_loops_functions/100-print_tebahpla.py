1926eb17939288fb4001d6cbd0cb5325c3461a70
Mohamed Rida Oukili <mridaoukili@gmail.com>
2024-09-10 07:16:55 -0700

    update

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 print("{}".format("".join(
-    chr(122 - i) if i % 2 == 0 else chr(90 - (i // 2))
+    chr(122 - i // 2) if i % 2 == 0 else chr(90 - i // 2)
     for i in range(26)
 )), end="")
