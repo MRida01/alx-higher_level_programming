29d290883d9c7f551f714c5ec6229c2ef4d5568d
Mohamed Rida Oukili <mridaoukili@gmail.com>
2024-09-10 06:38:38 -0700

    update

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 
-print("".join([chr(char) for char in range(ord('a'), ord('z') + 1)]), end="")
+print("".join([chr(char) for char in range(ord('a'), ord('z') + 1)]).format(), end="")
