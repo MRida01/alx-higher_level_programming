55878806440cce122a6f2c2457bf7daab41c0e6b
Mohamed Rida Oukili <mridaoukili@gmail.com>
2024-09-10 08:27:55 -0700

    updates

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
-number = 98
-print(f"{number} Battery street")
+
+def print_battery_street(value):
+    try:
+        number = int(value)
+        print(f"{number} Battery street")
+    except ValueError:
+        print("Error: Input must be an integer.")
